993fd34639005a9c1e33c404194cb9ca18378212
Erico-Araujo <122834091+Erico2451@users.noreply.github.com>
2023-03-26 23:41:36 -0300

    Sagagames3.0

---
+++ TRABALHOPHPmain/saga/adm/adm.php
@@ -0,0 +1,37 @@
+<?php
+
+
+include "../adm/controle.php";
+include "../adm/seguranca_adm.php";
+
+?>
+<div class="container p-5">
+    <div class="row text-center quadro">
+        <div class="col-12 col-md-6">
+            <a href="">
+                <img src="" alt="">
+                <p>USUARIO</P>
+</a>
+</div>
+
+<div class="col-12 col-md-6">
+            <a href="">
+                <img src="" alt="">
+                <p>ADM</p>
+</a>
+</div>
+
+<div class="col-12 col-md-6">
+            <a href="">
+                <img src="" alt="">
+                <p>EDITAR</p>
+</a>
+</div>
+
+<div class="col-12 col-md-6">
+            <a href="">
+                <img src="" alt="">
+                <p>PRODUTO</p>
+</a>
+</div>
+</div>
---
+++ TRABALHOPHPmain/saga/adm/rodape.php
@@ -0,0 +1,14 @@
+
+</div>
+
+<div class="container-fluid bg-dark text-white p-3 fixed mt-5">
+    <div class="row">
+        <div class="col text-center">
+            copyright 2023 - Saga Games
+          </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
